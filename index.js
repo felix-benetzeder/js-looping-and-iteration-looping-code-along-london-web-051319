73d1099b5862767befb59c7a06d4b3c375390de1
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-22 22:05:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(person, event) {
+  let cards = []
+  for (let i = 0; i < person.length; i++) {
+    cards.push(`Thank you, ${person[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+
+function countdown(number){
+  while (number >= 0){
+    console.log(number);
+    number--;
+  }
+}
